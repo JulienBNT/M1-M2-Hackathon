a4594376871413eda80f1dc3eee8b35da00da909
JulienBNT <julien.binet91@gmail.com>
2025-03-10 16:47:27 +0100

    CRUD users: profile, modify, delete routes

--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const router = express.Router();
-const { registerUser, loginUser } = require("../controllers/userController");
+const { registerUser, loginUser, getUserProfile, updateUserProfile, deleteUserAccount } = require("../controllers/userController");
 
 router.post("/register", registerUser);
 router.post("/login", loginUser);
+router.get("/:id", getUserProfile);
+router.put("/:id", updateUserProfile);
+router.delete("/:id", deleteUserAccount);
 
 module.exports = router;
