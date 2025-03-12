c9e0c8818b8d796563e86a527ded8ec9ee58b282
JulienBNT <julien.binet91@gmail.com>
2025-03-12 09:33:34 +0100

    DEV-10/middlewares

--- backend/routes/postRoute.js
+++ backend/routes/postRoute.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const router = express.Router();
 const { createPost, deletePost, viewPost, getAllPosts, modifyPost } = require('../controllers/postController');
+const { protect } = require('../middlewares/authMiddleware');
 
-router.post('/', createPost);
-router.delete('/:id', deletePost);
-router.get('/:id', viewPost);
-router.get('/', getAllPosts);
-router.put('/:id', modifyPost);
+router.post('/', protect, createPost);
+router.delete('/:id', protect, deletePost);
+router.get('/:id', protect, viewPost);
+router.get('/', protect, getAllPosts);
+router.put('/:id', protect, modifyPost);
 
 module.exports = router;
